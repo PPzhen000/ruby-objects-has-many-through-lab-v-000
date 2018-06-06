880231e5fd7b833ec9549c5f10472f12afa5e1aa
Ariel Zhen <zhenpingping@live.com>
2018-06-06 21:31:45 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,8 +2,23 @@ class Genre
 
   @@all_genres = []
 
-  def initialize
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+  def self.all?
+    @@all
+  end
+
+  def genre
 
   end
-  
+
+  def artist
+
+  end
+
 end
