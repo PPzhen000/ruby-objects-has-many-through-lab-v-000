05d5d64b919c9325c8612ad3e7eacee8334c5709
Ariel Zhen <zhenpingping@live.com>
2018-06-06 22:34:18 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -19,10 +19,10 @@ class Patient
 
   def appointments
     Appointment.all.select do |appointment|
-      appointment.patient == self 
-    end 
-  end 
-  
+      appointment.patient == self
+    end
+  end
+
   def doctors
     appointments.map do |appointment|
       appointment.doctor
