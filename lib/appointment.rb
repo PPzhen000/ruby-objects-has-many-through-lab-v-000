1d9ef115c53d3d01bec7d1e6897be73b52bc9b7f
Ariel Zhen <zhenpingping@live.com>
2018-06-06 22:21:51 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,4 +1,25 @@
 class Appointment
 
+  @@all_appointments = [] 
+
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date 
+    @@all_appointments << self 
+  end 
+
+  def self.all 
+    @@all_appointments 
+  end 
+
+  def patient 
+    @patient 
+  end 
+
+  def doctor 
+    @doctor 
+  end 
+    
 
 end 
