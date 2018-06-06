f08d80c56471ed20d3a2526a978d8727731488a2
Ariel Zhen <zhenpingping@live.com>
2018-06-06 22:27:34 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,20 @@
 class Patient
 
+  attr_accessor :name
 
-end 
+  @@all_patients = []
+
+  def initialize(name)
+    @name = name
+    @@all_patients << self
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+
+  def doctors
+
+  end
+  
+end
