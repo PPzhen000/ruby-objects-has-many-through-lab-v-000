5ce5a61c2d4905c07045173802c8daaa5afda2ed
Ariel Zhen <zhenpingping@live.com>
2018-06-06 21:34:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,14 @@
 class Song
 
+  attr_accessor :name, :artist, :genre
+
   @@all_songs = []
 
-  def initialize
-
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@all << self
   end 
 
 end
